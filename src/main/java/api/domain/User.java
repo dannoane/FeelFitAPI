a2e98a6c1dc14c1a363328c99737937cf59d3eb6
Noane Dan <Noane.Dan@outlook.com>
2018-02-20 19:13:37 +0200

    Domain

---
+++ src/main/java/api/domain/User.java
@@ -0,0 +1,48 @@
+package api.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.Id;
+import org.springframework.hateoas.ResourceSupport;
+
+public class User extends ResourceSupport {
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
