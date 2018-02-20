95e6be50e4cd7054c5ad69097894dc3ca85a50eb
Noane Dan <Noane.Dan@outlook.com>
2018-02-20 19:14:11 +0200

    Controllers

---
+++ src/main/java/api/controller/UserController.java
@@ -0,0 +1,43 @@
+package api.controller;
+
+import api.domain.User;
+import api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+@RepositoryRestController
+public class UserController {
+
+    private final UserRepository repository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserController(UserRepository repository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.repository = repository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/users")
+    public @ResponseBody HttpEntity<User> signUp(@RequestBody User user) {
+
+        String encodedPassword;
+
+        encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        User newUser = repository.save(user);
+        newUser.add(linkTo(methodOn(UserController.class).signUp(newUser)).withSelfRel());
+
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+}
