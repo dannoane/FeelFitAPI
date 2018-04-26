4b1947fd988a71859f28d23f52bce2ff1c1e2a52
Noane Dan <Noane.Dan@outlook.com>
2018-04-26 11:06:05 +0300

    Use lombok annotations

--- src/main/java/api/domain/User.java
+++ src/main/java/api/domain/User.java
@@ -1,48 +1,17 @@
 package api.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.hateoas.ResourceSupport;
 
+@ToString
 public class User extends ResourceSupport {
 
-    @Id
-    private String id;
-    private String username;
-    private String password;
-    private String name;
-    private String email;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    @Id private String id;
+    @Getter @Setter private String username;
+    @Getter @Setter private String password;
+    @Getter @Setter private String name;
+    @Getter @Setter private String email;
 }
