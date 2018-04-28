205cfb76ae0672d391be7aeb0422f710257dcf6c
Noane Dan <Noane.Dan@outlook.com>
2018-04-28 16:27:02 +0300

    Add workout controller and repository methods for it

---
+++ src/main/java/api/controller/WorkoutController.java
@@ -0,0 +1,63 @@
+package api.controller;
+
+import api.domain.Workout;
+import api.repository.WorkoutRepository;
+import api.security.JWTSubject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RepositoryRestController
+public class WorkoutController {
+
+    private final WorkoutRepository workoutRepository;
+
+    @Autowired
+    public WorkoutController(WorkoutRepository workoutRepository) {
+        this.workoutRepository = workoutRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/workouts/me")
+    public @ResponseBody HttpEntity<List<Workout>> readMyWorkouts() {
+
+        List<Workout> workouts = workoutRepository
+                .findByUsername(JWTSubject.getSubject());
+
+        if (workouts.size() == 0) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(workouts, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/workouts")
+    public @ResponseBody HttpEntity<List<Workout>> readWorkouts(@RequestParam String username) {
+
+        List<Workout> workouts = workoutRepository
+                .findByUsernameRegex(username);
+
+        if (workouts.size() == 0) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(workouts, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/workouts")
+    public @ResponseBody HttpEntity<Workout> createWorkout(@RequestBody Workout workout) {
+
+        if (workout.getRoute() == null || workout.getStatistics() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        workout.setUsername(JWTSubject.getSubject());
+        Workout savedWorkout = workoutRepository.save(workout);
+
+        return new ResponseEntity<>(savedWorkout, HttpStatus.OK);
+    }
+}
--- src/main/java/api/repository/WorkoutRepository.java
+++ src/main/java/api/repository/WorkoutRepository.java
@@ -2,8 +2,18 @@ package api.repository;
 
 import api.domain.Workout;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(collectionResourceRel = "workouts", path = "workouts")
+import java.util.List;
+
+@Repository
 public interface WorkoutRepository extends MongoRepository<Workout, String> {
+
+    @Query(value = "{ 'username': ?0 }")
+    List<Workout> findByUsername(@Param("username") String username);
+
+    @Query(value = "{ 'username': { $regex: '?0', $options: 'i' } }")
+    List<Workout> findByUsernameRegex(@Param("username") String username);
 }
