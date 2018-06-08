df00de5ead6d5fdcfa38cbbc1106b8cc04dbbf90
Noane Dan <Noane.Dan@outlook.com>
2018-06-08 16:45:26 +0300

    Create 2dsphere index

--- src/main/java/api/domain/Location.java
+++ src/main/java/api/domain/Location.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
 import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,7 +14,7 @@ public class Location {
     @Id
     private ObjectId id;
 
-    @Getter @Setter @GeoSpatialIndexed
+    @Getter @Setter @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
     private double[] point;
 
     @Getter @Setter
